da4ae65b5b223fd98849b234dfa4646b753e82e8
leandro elverdi <leandroelverdi@gmail.com>
2025-06-12 20:58:24 -0300

    add arrow scrollToTop

--- js/scrollToTop.js
+++ js/scrollToTop.js
@@ -1,18 +1,7 @@
 const btn = document.getElementById("scrollToTopBtn");
-
-// Mostrar el botón cuando se hace scroll hacia abajo
-window.onscroll = function () {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        btn.style.display = "flex";
-    } else {
-        btn.style.display = "none";
-    }
-};
-
-// Scroll suave hacia arriba al hacer clic
-btn.onclick = function () {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-};
+window.addEventListener("scroll", function () {
+    btn.style.display = window.scrollY > 200 ? "flex" : "none";
+});
+btn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
